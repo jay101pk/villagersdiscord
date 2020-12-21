7168e1636dd796d4797e6d3948e3b08ba091ef1f
Jason Logerquist <jasonlogerquist@comcast.net>
2020-12-20 22:36:06 -0600

    add pics to home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,13 @@
 import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
 import { PrismaClient } from "@prisma/client"
 
 const prisma = new PrismaClient()
+
+const discord_link = "https://cdn.discordapp.com/"
 
 export default function Home({servers}) {
   return (
@@ -16,8 +20,10 @@ export default function Home({servers}) {
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to website for a discord bot </h1>
-          {servers.map(({server, server_id}) => (
-            <a className={styles.description} href={"/servers/" + server_id} key={server_id}>{server}</a>
+          {servers.map(({server, serverId, serverIcon}) => (
+            <div>
+              <Link href={"/servers/" + serverId}><Image src={discord_link + "icons/" + serverId + "/"+ serverIcon+".png"} alt={server + "'s icon"} width={250} height={250}/></Link>
+            </div>
           ))}
       </main>
     </div>
@@ -26,11 +32,14 @@ export default function Home({servers}) {
 
 export async function getStaticProps() {
   const servers = (await prisma.servers.findMany()).map(server => {
+
     return {
       server: server.name,
-      server_id: server.id
+      serverId: server.id.toString(),
+      serverIcon: server.icon
     }
   })
+  prisma.$disconnect()
   return {
     props: {
       servers
